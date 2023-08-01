43161139390c39a80b2a867736acaea296d111f0
bbkx226 <bbkx226@gmail.com>
2023-08-01 09:09:33 +0800

    Done Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
+import SignInButton from '@/components/SignInButton';
 import { Button } from '@/components/ui/button';
-import { prisma } from '@/lib/prisma';
-
-export default function Home() {
-  return <Button>Hello World</Button>
+import { Card, CardDescription, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { getAuthSession } from '@/lib/nextauth';
+import { redirect } from 'next/navigation'
+export default async function Home() {
+  const session = await getAuthSession()
+  if (session?.user) {
+    return redirect('/dashboard')
+  }
+  return (
+  <div className='absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'>
+      <Card>
+        <CardHeader>
+          <CardTitle>Welcome to QuizBraniac!</CardTitle>
+          <CardDescription>
+            QuizBraniac is a quiz app that allows you to create and play quizzes.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton text="Sign In with Google!"/>
+        </CardContent>
+      </Card>
+  </div>
+  )
 }
