80d5f8bdd1f951360d663b72497c5b177153babb
bbkx226 <bbkx226@gmail.com>
2023-08-03 21:57:27 +0800

    Added Toast Bar for Notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import Providers from '@/components/Providers'
+import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Providers>          
           <Navbar />
           {children}
+          <Toaster />
         </Providers>  
       </body>
     </html>
