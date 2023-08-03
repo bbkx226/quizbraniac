7c4b36aff032859860c0754bf8d29cfc56aa8989
bbkx226 <bbkx226@gmail.com>
2023-08-03 21:58:10 +0800

    Added MCQ in-detailed components

--- src/schemas/form/quiz.tsx
+++ src/schemas/form/quiz.tsx
@@ -5,3 +5,8 @@ export const quizCreationSchemas = z.object({
     type: z.enum(['mcq', 'open_ended']),
     amount: z.number().min(1).max(10),
 })
+
+export const checkAnswerSchema = z.object({
+    questionId: z.string(),
+    userAnswer: z.string(),
+})
