206831898af19fc02edf6c694c8eb5350dc1c118
bbkx226 <bbkx226@gmail.com>
2023-08-05 22:42:41 +0800

    Improved UX

--- src/components/DetailsDialog.tsx
+++ src/components/DetailsDialog.tsx
@@ -64,7 +64,7 @@ const DetailsDialog = (props: Props) => {
             <hr />
             <p className="my-2 font-semibold">
               <h4 className="text-base font-semibold">My other projects</h4>
-              <div className="grid justify-around lg:grid-cols-4 md:grid-cols-1 mt-2 gap-y-3">
+              <div className="grid justify-around lg:grid-cols-4 mt-2 gap-y-3">
                 <a href="https://shirtify-me.vercel.app/" target="_blank">
                     <div className="flex items-center gap-2">
                     <Image
@@ -88,51 +88,6 @@ const DetailsDialog = (props: Props) => {
                     <span className="">Terminal Website</span>
                     </div>
                 </a>
-                <a href="https://imagegeneratorai-6e92d.web.app/" target="_blank">
-                    <div className="flex items-center gap-2">
-                    <Image
-                        alt="AI Image Generator"
-                        src="/imageaigenerator.jpg"
-                        width={80}
-                        height={80}
-                        style={{marginTop: "1.5rem"}}
-                    />
-                    <span className="">AI Image Generator</span>
-                    </div>
-                </a>
-                <a href="https://brancoin-crypto.vercel.app/" target="_blank">
-                    <div className="flex items-center gap-2">
-                    <Image
-                        alt="Brancoin"
-                        src="/bcstamp.png"
-                        width={80}
-                        height={80}
-                    />
-                    <span className="">BranCoin</span>
-                    </div>
-                </a>
-                <a href="https://ai-verse.vercel.app/" target="_blank">
-                    <div className="flex items-center gap-2">
-                    <Image
-                        alt="AI-verse"
-                        src="/aiverse.png"
-                        width={80}
-                        height={80}
-                    />
-                    <span className="">AI-verse</span>
-                    </div>
-                </a>
-                <a href="https://blockchainapp-4e293.web.app/" target="_blank">
-                    <div className="flex items-center gap-2">
-                    <Image
-                        alt="Opac1ty"
-                        src="/opacity.png"
-                        width={80}
-                        height={80}
-                    />
-                    <span className="">Opac1ty</span>
-                    </div>
-                </a>
                 <a href="https://brandongpt-summarizer.web.app/" target="_blank">
                     <div className="flex items-center gap-2">
                     <Image
@@ -144,7 +99,53 @@ const DetailsDialog = (props: Props) => {
                     <span className="">AI Summarizer</span>
                     </div>
                 </a>
-                <a href="https://app.engati.com/static/standalone/bot.html?bot_key=63ef367dfa364830&env=p" target="_blank">
+                <a href="https://imagegeneratorai-6e92d.web.app/" target="_blank" className="hidden sm:block">
+                    <div className="flex items-center gap-2">
+                    <Image
+                        alt="AI Image Generator"
+                        src="/imageaigenerator.jpg"
+                        width={80}
+                        height={80}
+                        style={{marginTop: "1.5rem"}}
+                    />
+                    <span className="">AI Image Generator</span>
+                    </div>
+                </a>
+                <a href="https://brancoin-crypto.vercel.app/" target="_blank" className="hidden sm:block">
+                    <div className="flex items-center gap-2">
+                    <Image
+                        alt="Brancoin"
+                        src="/bcstamp.png"
+                        width={80}
+                        height={80}
+                    />
+                    <span className="">BranCoin</span>
+                    </div>
+                </a>
+                <a href="https://ai-verse.vercel.app/" target="_blank" className="hidden sm:block">
+                    <div className="flex items-center gap-2">
+                    <Image
+                        alt="AI-verse"
+                        src="/aiverse.png"
+                        width={80}
+                        height={80}
+                    />
+                    <span className="">AI-verse</span>
+                    </div>
+                </a>
+                <a href="https://blockchainapp-4e293.web.app/" target="_blank" className="hidden sm:block">
+                    <div className="flex items-center gap-2">
+                    <Image
+                        alt="Opac1ty"
+                        src="/opacity.png"
+                        width={80}
+                        height={80}
+                    />
+                    <span className="">Opac1ty</span>
+                    </div>
+                </a>
+
+                <a href="https://app.engati.com/static/standalone/bot.html?bot_key=63ef367dfa364830&env=p" target="_blank" className="hidden sm:block">
                     <div className="flex items-center gap-2">
                     <Image
                         alt="Guzheng Chatbot"
