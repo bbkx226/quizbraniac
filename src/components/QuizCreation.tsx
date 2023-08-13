ebcc540cb730d0eb217fb38914adf5eec32926ce
bbkx226 <bbkx226@gmail.com>
2023-08-13 21:02:14 +0800

    Modified auto-formatting times

--- src/components/QuizCreation.tsx
+++ src/components/QuizCreation.tsx
@@ -21,9 +21,10 @@ import { Input } from "@/components/ui/input"
 import { BookOpen, CopyCheck } from 'lucide-react'
 import { Separator } from './ui/separator'
 import { useMutation } from '@tanstack/react-query'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { useRouter } from 'next/navigation'
 import LoadingQuestions from './LoadingQuestions'
+import { useToast } from "./ui/use-toast";
 
 type Props = {
     topicParam: string
@@ -35,6 +36,7 @@ const QuizCreation = ({ topicParam }: Props) => {
     const router = useRouter()
     const [showLoader, setShowLoader] = React.useState(false)
     const [finished, setFinished] = React.useState(false)
+    const { toast } = useToast();
     const {mutate: getQuestions, isLoading} = useMutation({
         mutationFn: async ({ amount, topic, type }: Input) => {
             const response = await axios.post('/api/game', {
@@ -55,112 +57,129 @@ const QuizCreation = ({ topicParam }: Props) => {
         }
     })
 
-    function onSubmit(input: Input) {
-        setShowLoader(true)
-        getQuestions({
-            amount: input.amount,
-            topic: input.topic,
-            type: input.type
-        }, {
-            onSuccess: ({ gameId }) => {
-                setFinished(true)
-                setTimeout(() => {
-                    if(form.getValues('type') == 'open_ended'){
-                        router.push(`/play/open-ended/${gameId}`)
-                    } else {
-                        router.push(`/play/mcq/${gameId}`)
-                    }
-                }, 1000)
-            },
-            onError: () => {
-                setShowLoader(false)
+    const onSubmit = async (data: Input) => {
+        setShowLoader(true);
+        getQuestions(data, {
+          onError: (error) => {
+            setShowLoader(false);
+            if (error instanceof AxiosError) {
+              if (error.response?.status === 500) {
+                toast({
+                  title: "Error",
+                  description: "Something went wrong. Please try again later.",
+                  variant: "destructive",
+                });
+              }
             }
-        })
-    }
+          },
+          onSuccess: ({ gameId }: { gameId: string }) => {
+            setFinished(true);
+            setTimeout(() => {
+              if (form.getValues("type") === "mcq") {
+                router.push(`/play/mcq/${gameId}`);
+              } else if (form.getValues("type") === "open_ended") {
+                router.push(`/play/open-ended/${gameId}`);
+              }
+            }, 2000);
+          },
+        });
+      };
+      form.watch();
+    
+      if (showLoader) {
+        return <LoadingQuestions finished={finished} />;
+      }
 
-    form.watch();
-    if(showLoader) {
-        return <LoadingQuestions finished={finished}/>
-    }
-
-  return (
+    return (
     <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
         <Card>
-            <CardHeader>
-                <CardTitle className="font-bold text-2x">Quiz Creation</CardTitle>
-                <CardDescription>Choose a topic</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                        <FormField
-                        control={form.control}
-                        name="topic"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Topic</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Enter a topic..." {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    Please provide a topic
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        )}
+        <CardHeader>
+            <CardTitle className="text-2xl font-bold">Quiz Creation</CardTitle>
+            <CardDescription>Choose a topic</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                control={form.control}
+                name="topic"
+                render={({ field }) => (
+                    <FormItem>
+                    <FormLabel>Topic</FormLabel>
+                    <FormControl>
+                        <Input placeholder="Enter a topic" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                        Please provide any topic you would like to be quizzed on
+                        here.
+                    </FormDescription>
+                    <FormMessage />
+                    </FormItem>
+                )}
+                />
+                <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                    <FormItem>
+                    <FormLabel>Number of Questions</FormLabel>
+                    <FormControl>
+                        <Input
+                        placeholder="How many questions?"
+                        type="number"
+                        {...field}
+                        onChange={(e) => {
+                            form.setValue("amount", parseInt(e.target.value));
+                        }}
+                        min={1}
+                        max={10}
                         />
-                        <FormField
-                            control={form.control}
-                            name="amount"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Number of Questions</FormLabel>
-                                    <FormControl>
-                                        <Input 
-                                            placeholder="Enter an amount..."
-                                            {...field}
-                                            type='number'
-                                            min={1}
-                                            max={10}
-                                            onChange={e => {
-                                                form.setValue('amount', parseInt(e.target.value))
-                                            }}
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <div className="flex justify-between">
-                            <Button
-                                type='button'
-                                onClick={() => {
-                                    form.setValue('type', 'mcq')
-                                }}
-                                className='w-1/2 rounded-none rounded-l-lg' 
-                                variant={form.getValues('type') === 'mcq' ? 'default' : 'secondary'}
-                            >
-                                <CopyCheck className='m-4 h-4 mr-2'/>Multiple Choice 
-                            </Button>
-                            <Separator orientation='vertical' />
-                            <Button
-                                type='button'
-                                onClick={() => {
-                                    form.setValue('type', 'open_ended')
-                                }}
-                                className='w-1/2 rounded-none rounded-r-lg' 
-                                variant={form.getValues('type') === 'open_ended' ? 'default' : 'secondary'}
-                            >
-                                <BookOpen className='w-4 h-4 mr-2' />Open Ended 
-                            </Button>
-                        </div>
-                        <Button disabled={isLoading} type="submit">Submit</Button>
-                    </form>
-                </Form>
-            </CardContent>
+                    </FormControl>
+                    <FormDescription>
+                        You can choose how many questions you would like to be
+                        quizzed on here.
+                    </FormDescription>
+                    <FormMessage />
+                    </FormItem>
+                )}
+                />
+
+                <div className="flex justify-between">
+                <Button
+                    variant={
+                    form.getValues("type") === "mcq" ? "default" : "secondary"
+                    }
+                    className="w-1/2 rounded-none rounded-l-lg"
+                    onClick={() => {
+                    form.setValue("type", "mcq");
+                    }}
+                    type="button"
+                >
+                    <CopyCheck className="w-4 h-4 mr-2" /> Multiple Choice
+                </Button>
+                <Separator orientation="vertical" />
+                <Button
+                    variant={
+                    form.getValues("type") === "open_ended"
+                        ? "default"
+                        : "secondary"
+                    }
+                    className="w-1/2 rounded-none rounded-r-lg"
+                    onClick={() => form.setValue("type", "open_ended")}
+                    type="button"
+                >
+                    <BookOpen className="w-4 h-4 mr-2" /> Open Ended
+                </Button>
+                </div>
+                <Button disabled={isLoading} type="submit">
+                Submit
+                </Button>
+            </form>
+            </Form>
+        </CardContent>
         </Card>
     </div>
-  )
+    );
 }
 
 export default QuizCreation
--- src/lib/gpt.ts
+++ src/lib/gpt.ts
@@ -17,7 +17,7 @@ export async function strict_output(
   output_value_only: boolean = false,
   model: string = "gpt-3.5-turbo",
   temperature: number = 1,
-  num_tries: number = 50,
+  num_tries: number = 7,
   verbose: boolean = false
 ): Promise<
   {
