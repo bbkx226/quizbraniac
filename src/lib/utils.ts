d2856eab9e41a2c22e0b403dc8e1b7f2f6e69446
bbkx226 <bbkx226@gmail.com>
2023-08-18 16:19:18 +0800

    Update comments

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,17 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
+// The function is named cn and uses the spread syntax (...) to allow passing multiple arguments.
+// ClassValue[]: ClassValue is a type (which might be imported from a CSS-in-JS library or defined elsewhere) that represents a CSS class name or an object of class names and their conditions. 
+// ClassValue[] indicates that the function accepts an array of such values.
+// `clsx()` conditionally concatenate class names
+// twMerge() a function from a library like "twin.macro" (a popular utility for styling in React with Tailwind CSS) that processes and optimizes class names for better performance and compatibility.
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+// push(): Adds one or more elements to the end of an array and returns the new length of the array
+// join(): Combine all the elements of an array into a single string. 
 export function formatTimeDelta(seconds: number) {
   const hours = Math.floor(seconds / 3600)
   const minutes = Math.floor((seconds - hours * 3600) / 60)
