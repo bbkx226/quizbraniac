d7e14c0a55b6972301db2713cdf3e3fe0fef5209
bbkx226 <bbkx226@gmail.com>
2023-08-19 14:23:08 +0800

    Update Comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ export default async function Home() {
   }
   
   return (
+  // The combined effect is that the element is centered horizontally and vertically within its parent
   <div className='absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'>
       <Card>
         <CardHeader>
--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,17 +1,24 @@
-'use client'
+'use client' // Runs this code only on client side.
 
 import React from 'react'
 import { Button } from './ui/button'
 import { signIn } from 'next-auth/react'
 
-type Props = {
+type Props = { // Defines Props interface for component props.
     text: string
 }
 
-const SignInButton = ({text}: Props) => {
+const SignInButton = ({ text }: Props) => {
   return (
-    <Button onClick={()=>{
-        signIn('google').catch(console.error)
+    // Using signIn() directly would call it on every render.
+    // The arrow function () => {signIn()} is only invoked when clicked.
+
+    // the arrow function avoids:
+    // Immediately invoking on render
+    // Losing component context
+    // Needing to pass args to signIn()
+    <Button onClick={()=>{ // Button click calls next-auth signIn function.
+        signIn('google').catch(console.error) // initiates Google OAuth login flow, signIn returns promise to handle success/failure.
     }}>
         {text}
     </Button>
