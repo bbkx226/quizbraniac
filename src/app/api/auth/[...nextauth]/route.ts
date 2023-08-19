19588131fee8fe92e14e9abc5d2aa69074d27375
bbkx226 <bbkx226@gmail.com>
2023-08-19 12:29:27 +0800

    Update Comments

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
-import { authOptions } from "@/lib/nextauth";
+import { authOptions } from "@/lib/nextauth"; // Imports the authOptions object that contains the NextAuth.js configuration
 import NextAuth from "next-auth/next";
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST } // NextAuth needs both GET and POST handlers, for handling OAuth callbacks for example
+// The exported handlers contain all the logic for session handling, OAuth callbacks, validating JWTs etc. 
+// We can just use them to protect API routes and get user sessions.
